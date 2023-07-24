a6ec6aa6877106b5cec17a8f4ac5452ab7595f74
Jacky <jchoi53@mail.ccsf.edu>
2023-07-23 21:47:44 -0700

    add more exercise programs week 3

---
+++ cplusplus/test/test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    int y;
+
+    /* x = 5;
+
+    cout<<x + 2;
+    y = x + 2;
+    x + 2;
+
+    cout<<x;
+    cout<<x-2;
+    y = x * 2;
+
+    cout<<x / 2;
+    cout<<x % 2; */
+
+    x = 6;
+    cout<<x + 7;
+    y = (x = 8);
+    y = x = 9;
+
+    return 0;
+} 
