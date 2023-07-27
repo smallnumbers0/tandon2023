743722b1af7834caaf140f6054f06c03f2f9828f
Jacky <jchoi53@mail.ccsf.edu>
2023-07-26 17:21:19 -0700

    finish q4

--- week3/hw3/q4/jc12948_hw3_q4.cpp
+++ week3/hw3/q4/jc12948_hw3_q4.cpp
@@ -12,3 +12,82 @@ Implementation requirements:
 1. Use a switch statement.
 2. You are not allowed to include and use the math library.
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double userInput, decimal;
+    int roundMethod, roundedInput;
+    int temp; //turn userInput into int to remove decimal
+
+    cout<<"Please enter a Real Number: "<<endl;
+    cin>>userInput;
+    cout<<"Choose your rounding method: "<<endl;
+    cout<<"1. Floor round\n2. Ceiling round\n3. Round to Nearest Integer"<<endl;
+    cin>>roundMethod;
+
+    switch(roundMethod) {
+        case 1: 
+            //temp = userinput
+            //if userinput  >= 0 { decimal = userinput - temp } else  decimal = temp - userinput
+            //roundednumber = temp
+            temp = userInput;
+            if(userInput >= 0) {
+                decimal = userInput - temp;
+            }
+            else {
+                decimal = temp - userInput;
+            }
+            
+            if(temp < 0 && decimal > 0) {
+                roundedInput = temp - 1;
+            }
+            else {
+                roundedInput = temp;
+            }
+            cout<<roundedInput<<endl;
+            break;
+        
+        case 2:
+            temp = userInput;
+            if(userInput >= 0) {
+                decimal = userInput - temp;
+            }
+            else {
+                decimal = temp - userInput;
+            }
+            
+            if(temp >= 0 && decimal > 0) {
+                roundedInput = temp + 1;
+            }
+            else {
+                roundedInput = temp;
+            }
+            cout<<roundedInput<<endl;
+            break;
+            
+
+        case 3:
+            temp = userInput;
+            if(userInput >= 0) {
+                decimal = userInput - temp;
+            }
+            else {
+                decimal = temp - userInput;
+            }
+
+            if(decimal >= 0.5 && temp >= 0) {
+                roundedInput = temp + 1;
+            }
+            else if(decimal >= 0.5 && temp < 0)  {
+                roundedInput = temp - 1;
+            }
+            else roundedInput = temp;
+            cout<<roundedInput<<endl;
+            break;
+            
+    }
+
+
+}
