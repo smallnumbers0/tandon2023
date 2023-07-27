f2ed8a79d1cc54c5221f2f58847a8abc34cf67a0
Jacky <jchoi53@mail.ccsf.edu>
2023-07-27 11:51:53 -0700

    q6 finish

--- week3/hw3/q6/jc12948_hw3_q6.cpp
+++ week3/hw3/q6/jc12948_hw3_q6.cpp
@@ -13,3 +13,53 @@ Notes:
 We Th Fr Sa Su
 3. The number of minutes will be input as a positive integer.
 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    const string SATURDAY = "Sa";
+    const string SUNDAY = "Su";
+    const int MINUTES_IN_ONE_HOUR = 60;
+    const int EIGHT_AM = 8;
+    const int SIX_PM = 18;
+    const double WEEKEND_RATE = 0.15;
+    const double TIME_DISCOUNT_RATE = 0.25;
+    const double TIME_REGULAR_RATE = 0.40;
+
+    int callLength, hours, minutes, startTime;
+    char colon;
+    string day;
+    double rate;
+    double totalCost;
+
+    
+
+    cout<<"Please enter the day of the week (Mo, Tu, We, Th, Fr, Sa, Su): "<<endl;
+    cin>>day;
+    cout<<"Please enter the time when call started in 24-hour format (18:30 for 6:30 pm): "<<endl;
+    cin>>hours>>colon>>minutes;
+    cout<<"Please enter duration of call in minutes: "<<endl;
+    cin>>callLength;
+
+    startTime = (hours * MINUTES_IN_ONE_HOUR) + minutes;
+
+    if(day == "Sa" || day == "Su") {
+        rate = WEEKEND_RATE;
+    }
+    else {
+
+        if((startTime < EIGHT_AM * MINUTES_IN_ONE_HOUR) || (startTime > SIX_PM * MINUTES_IN_ONE_HOUR)) {
+            rate = TIME_DISCOUNT_RATE;
+        }
+        else {
+            rate = TIME_REGULAR_RATE;
+        }
+    }
+
+totalCost = callLength * rate;
+cout<<totalCost<<endl;
+
+}
