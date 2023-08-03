8bf7ceb1cc0e2f463dc1dd0f6916d1fad1103f17
Jacky <jchoi53@mail.ccsf.edu>
2023-08-02 22:59:22 -0700

    finish hw 4

--- week4/hw/codinghw/jc12948_hw4_q4.cpp
+++ week4/hw/codinghw/jc12948_hw4_q4.cpp
@@ -3,9 +3,49 @@ Write two versions of a program that reads a sequence of positive integers from 
 calculates their geometric mean, and print the geometric mean. 
 */
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-int main() {
+// int main() {
 
-    return 0;
+//     int product = 1;
+//     int input;
+//     double seqLength, geometricMean;
+
+//     cout<<"Please enter the length of the sequence: "<<endl;
+//     cin>>seqLength;
+
+//     //get number of inputs according to sequence length
+//     cout<<"Please enter your sequence of positive integers: "<<endl;
+
+//     for(int i = 0; i < seqLength; i++) {
+//         cin>>input;
+//         product *= input;
+//     }
+
+//     geometricMean = pow(product, (1/seqLength));
+//     cout<<geometricMean<<endl;
+
+//     return 0;
+// }
+
+int main() {
+    
+    int product = 1; 
+    int input;
+    double seqLength, geometricMean;
+
+        cout<<"Please enter a non-empty sequence of positive integers, each one in a seperate line. End your sequence by typing -1: "<<endl;
+        cin>>input;
+        while(input != -1) {
+            product *= input;
+            seqLength++;
+            cin>>input;
+        }
+
+        geometricMean = pow(product, (1/seqLength));
+        cout<<geometricMean<<endl;
+
+        return 0;
 }
