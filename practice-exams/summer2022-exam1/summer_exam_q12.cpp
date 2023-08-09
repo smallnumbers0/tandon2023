4d751d8995e28119a2233d027facb14b5ce9a011
Jacky <jchoi53@mail.ccsf.edu>
2023-08-09 09:48:13 -0700

    finish q12

--- practice-exams/summer2022-exam1/summer_exam_q12.cpp
+++ practice-exams/summer2022-exam1/summer_exam_q12.cpp
@@ -4,7 +4,44 @@ using namespace std;
 int main() {
 
     int input;
+    cout<<"enter an integer: "<<endl;
+    cin>>input;
 
-    
+    for(int i = 1; i <= 2 * input; i++) {
+        cout<<"*";
+    }
+    for(int i = 0; i < input; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout<<"*";
+        }
+        for(int j = i; j < input; j++) {
+            cout<<" ";
+        }
+        for(int j = i; j < input; j++) {
+            cout<<" ";
+        }
+         for(int j = 1; j <= i; j++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+     for(int i = 0; i < input; i++) {
+        for(int j = i; j <= input - 1; j++) {
+            cout<<"*";
+        }
+        for(int j = 1; j <= i; j++) {
+            cout<<" ";
+        }
+        for(int j = 1; j <= i; j++) {
+            cout<<" ";
+        }
+        for(int j = i; j <= input - 1; j++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+     for(int i = 1; i <= 2 * input; i++) {
+        cout<<"*";
+    }
     return 0;
 }
