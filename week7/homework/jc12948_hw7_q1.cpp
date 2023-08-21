f4481d85d78e43a1ee585f31a7992b26ce73b1f8
Jacky <jchoi53@mail.ccsf.edu>
2023-08-20 22:56:38 -0700

    post questions

---
+++ week7/homework/jc12948_hw7_q1.cpp
@@ -0,0 +1,12 @@
+/*
+Question 1: Print Monthly Calender Function
+Jacky Choi 08/20/2023
+jc12948 hw 7 q1
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    return 0;
+}
---
+++ week7/homework/jc12948_hw7_q2.cpp
@@ -0,0 +1,16 @@
+/*
+Question 2: Divisors and Perfect Number Functions
+Jacky Choi 08/20/2023
+jc12948 hw 7 q2
+*/
+
+#include <iostream>
+using namespace std;
+
+void analyzeDivisors (int num, int& outCountDivs, int& outSumDivs);
+bool isPerfect(int num);
+
+int main(){
+    cout<<"hello"<<endl;
+    return 0;
+}
