f424695dd5b6437cc7930c9d3c329dadb55e39af
Jacky <jchoi53@mail.ccsf.edu>
2023-08-24 19:45:30 -0700

    working input

--- week7/homework/jc12948_hw7_q2.cpp
+++ week7/homework/jc12948_hw7_q2.cpp
@@ -1,5 +1,5 @@
 /*
-Question 2: Divisors and Perfect Number Functions
+Question 2: Divisors and Perfect Number Functions w/ O(sqrt(n))
 Jacky Choi 08/20/2023
 jc12948 hw 7 q2
 */
@@ -10,7 +10,11 @@ using namespace std;
 void analyzeDivisors (int num, int& outCountDivs, int& outSumDivs);
 bool isPerfect(int num);
 
-int main(){
-    cout<<"hello"<<endl;
+int main() { 
+    int input;
+    cout<<"Please enter a positive integer greater than or equal to 2: "<<endl;
+    cin>>input;
+
+    cout<<input<<endl;
     return 0;
 }
