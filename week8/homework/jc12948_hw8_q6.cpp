53f262ee9c42c4755cd26dbe70811c1f263237ae
Jacky <jchoi53@mail.ccsf.edu>
2023-09-01 16:56:54 -0700

    finish q6

--- week8/homework/jc12948_hw8_q6.cpp
+++ week8/homework/jc12948_hw8_q6.cpp
@@ -5,41 +5,51 @@
 #include <string>
 using namespace std;
 
-char convertIntegers(char temp);
+string convertToX(string word);
+bool isDigit(string word);
 
 int main() {
-
-    string input = "test"; 
-    string result = "well";
-    cout<<"Enter a string of text: "<<endl;
+    string input;
+    cout<<"Please enter a line of text: "<<endl;
     getline(cin, input);
 
-    for(int i = 0; i < input.length(); i++) {
-        int count = 0;
-        string temp = "";
-        if(input[i] == ' ') {
-            temp = input.substr(count, i);
-            cout<<temp<<endl;
-            count = i;
+    int count = 0;
+    string temp = "test";
+    for(int i = 0; i <= input.length(); i++) {
+        if(input[i] == ' ' || i == input.length()) {
+            temp = input.substr(count, i - count);
+            cout<<convertToX(temp)<<" ";
+            count = i + 1;
         }
-        for(int i = 0; i < temp.length(); i++) {
-                if(temp[i] != '1' || temp[i] != '2' || temp[i] != '3' || temp[i] != '4' || temp[i] != '5' || temp[i] != '6' || temp[i] != '7' || temp[i] != '8' || temp[i] != '9' || temp[i] != '0') {
-                    temp[i] = temp[i];
-                }
-                else {
-                    temp[i] = convertIntegers(temp[i]) + ' ';
-                }
-        }
-        count++;
-        result += temp;
     }
-    cout<<result<<endl;
+    cout<<endl;
+
     return 0;
 }
-char convertIntegers(char temp) {
-    return temp = 'x';
-    
+
+bool isDigit(string word) {
+    for(int i = 0; i < word.length(); i++) {
+       if (word[i] < '0' || word[i] > '9') {
+            return false;
+        }
+    }
+    return true;
 }
+
+string convertToX(string word) {
+    
+    if(isDigit(word) == true) {
+        string convertWord;
+        for(int i = 0; i < word.length(); i++) {
+            convertWord += 'x';
+        }
+        return convertWord;
+    }
+    else {
+        return word;
+    }
+}
+
 //seperate whole string sentence by " "
 //find the words that only have integers in them
 //change all the words that only have integers into "x"s
