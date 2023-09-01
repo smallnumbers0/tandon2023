a05f98e832c80f005bdb227a5c05064c9e632327
Jacky <jchoi53@mail.ccsf.edu>
2023-09-01 14:21:25 -0700

    finish q5

---
+++ week8/homework/jc12948_hw8_q5.cpp
@@ -0,0 +1,14 @@
+//This program takes in a name in one format: First/Middle/Last and prints it out in another format: Last/First/Middle
+//Jacky Choi NYU Tandon Bridge Summer 2023
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string first, middle, last;
+
+    cout<<"Please enter your name in the following format seperated by a space: (First Middle Last): "<<endl;
+    cin>>first>>middle>>last;
+    middle = middle[0];
+    cout<<last<<", "<<first<<" "<<middle<<"."<<endl;
+}git
