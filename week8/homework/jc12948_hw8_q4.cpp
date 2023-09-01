df8738adcd84d4ccca7ddaadacd3def6d35ff141
Jacky <jchoi53@mail.ccsf.edu>
2023-09-01 14:14:06 -0700

    finish q4

--- week8/homework/jc12948_hw8_q4.cpp
+++ week8/homework/jc12948_hw8_q4.cpp
@@ -7,20 +7,32 @@
 using namespace std;
 
 int encryptedPin(int num[]);
+int pinEntry(int num[]);
 
 const int SECRET_PIN[5] = {8,1,1,4,5};
 
 int main() {
-    int input;
+    int num[10];
     srand(time(0));
+    int entry = pinEntry(num);
 
+    if (entry == encryptedPin(num)) {
+        cout << "Your pin is correct."<<endl;
+    } else {
+        cout << "Your pin is not correct."<<endl;
+    }
+
+
+    return 0;
+}
+
+int pinEntry(int num[]) {
+    int input;
     cout<<"Please enter the numbers corresponding to your pin: "<<endl;
     cout<<"Pin: 0 1 2 3 4 5 6 7 8 9"<<endl;
     cout<<"Num: ";
 
-    int num[10];
-
-    for(int i = 0; i < 10; i ++) {
+     for(int i = 0; i < 10; i ++) {
         num[i] = (rand() % 3) + 1;
         cout<<num[i]<<" ";
     }
@@ -29,5 +41,13 @@ int main() {
     cin>>input;
     cout<<input<<endl;
 
-    return 0;
+    return input;
+}
+
+int encryptedPin(int num[]) {
+    int encryptedPin = 0;
+    for(int i = 0; i < 5; i++) {
+        encryptedPin = 10 * encryptedPin + num[SECRET_PIN[i]];
+    }
+    return encryptedPin;
 }
