cea895f773405b2bf7d8f865dfa452d595aed1e8
Jacky <jchoi53@mail.ccsf.edu>
2023-09-02 14:53:25 -0700

    add week 9 materials

---
+++ week9/vectorEx.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> v;
+    for(int i = 0; i < 100; i++) {
+        v.push_back(i*100);
+    }
+    for(int i = 0; i < v.size(); i++) {
+        cout<<v[i]<<endl;
+    }
+}
