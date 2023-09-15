61f9a4dd6fe0325cf95563ca10fa8d76bd543eef
Jacky <jchoi53@mail.ccsf.edu>
2023-09-14 20:36:17 -0700

    fix range of values for q2

--- week10/homework/jc12948_hw10_q2.cpp
+++ week10/homework/jc12948_hw10_q2.cpp
@@ -1,15 +1,5 @@
+//This program takes an array and returns the numbers missing from the array from 0 to n.
 //Jacky Choi NYU Tandon Summer 2023
-// Question 2:
-// Implement the function:
-// int* findMissing(int arr[], int n, int& resArrSize)
-// This function gets an array of integers arr and its logical size n. All elements in arr are in the range {0, 1, 2, ... , n}.
-// Note that since the array contains n numbers taken from a range of size n+1, there must be at least one number that is missing (could be more than one number missing, if there are duplicate values in arr).
-// When called, it should create and return a new array, that contains all the numbers in range {0, 1, 2, ... , n} that are not in arr. The function should also update the output parameter, resArrSize, with the logical size of the new array that was created.
-// For example, if arr=[3, 1, 3, 0, 6, 4], after calling
-// findMissing(arr, 6, resArrSize), the function should create and return an array that contains [2, 5], and update the value in resArrSize to be 2.
-// Implementation requirements:
-// 1. Your function should run in linear time. That is, it should run in 𝜃(𝑛).
-// 2. Write a main() program that tests this function..
 
 #include <iostream>
 using namespace std;
@@ -18,7 +8,7 @@ int* findMissing(int arr[], int n, int& resArrSize);
 const int ARR_SIZE = 6;
 int main() {
     int newSize = 0;
-    int numArr[ARR_SIZE] = {3, 3, 3, 0, 6, 4};
+    int numArr[ARR_SIZE] = {0, 1, 2, 2, 4, 9};
 
     cout<<"Before: "<<endl;    
     cout<<"[";
@@ -43,21 +33,21 @@ int main() {
 
 int* findMissing(int arr[], int n, int& resArrSize) {
     int orderedCount[n + 1];
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i <= n; i++) {
         orderedCount[i] = 0;
     }
 
     for(int i = 0; i < n; i++) {
         orderedCount[arr[i]]++;
     }
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i <= n; i++) {
         if(orderedCount[i] == 0) {
             resArrSize++;
         }
     }
     int *missingNums = new int[resArrSize];
     int count = 0;
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i <= n; i++) {
         if(orderedCount[i] == 0) {
             missingNums[count] = i;
             count++;
