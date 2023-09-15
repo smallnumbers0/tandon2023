fd59286e357beab42a5ffc31b38bdf2ac92852e6
Jacky <jchoi53@mail.ccsf.edu>
2023-09-14 20:56:20 -0700

    create pointer array with input/output

--- week10/homework/jc12948_hw10_q3.cpp
+++ week10/homework/jc12948_hw10_q3.cpp
@@ -1,16 +1,7 @@
 //Jacky Choi NYU Tandon Summer 2023
 // Question 3:
 // In this question, you will write two versions of a program that reads from the user a sequence of positive integers ending with -1, and another positive integer num that the user wishes to search for.
-// The program should then print all the line numbers in sequence entered by the user, that contain num, or a message saying that num does not show at all in the sequence.
-// 13
-// 5
-// 8
-// 2
-// 9
-// 5
-// 8
-// 8
-// -1
+// The program should then print all the line numbers in sequence entered by the user, that contain num, or a message saying that num does not show at all in the sequence. 13 5 8 2 9 5 8 8 -1
 // Please enter a number you want to search. 5
 // 5 shows in lines 2, 6.
 // a) The first version of the program, is not allowed to use the vector data structure.
@@ -21,3 +12,36 @@
 // sequence, your program should run in 𝜃(𝑛).
 // 3. Write the two programs in two functions named main1() and main2(). Also have
 // the main() test these two functions.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void main1();
+
+int main() {
+    main1();
+    return 0;
+}
+
+void main1() { //This function does not use vector
+    int size = 0;
+    int* arr = new int[size];
+    //use a ptr to create dynamic memory heap
+    //we increment size for every input
+    //an input of -1 stops sequence
+    int input = 0;
+    while(input != -1) {
+        cin>>input;
+        if(input == -1) { 
+            break; //break here so -1 is not in array.
+        }
+        arr[size] = input;
+        size++;
+    }
+
+    for(int i = 0; i < size; i++) {
+        cout<<arr[i]<<endl;
+    }
+
+}
