7548baf082635191c07f20b55d84a936959f50fc
Jacky <jchoi53@mail.ccsf.edu>
2023-09-14 21:17:38 -0700

    start main2, vector and input output check

--- week10/homework/jc12948_hw10_q3.cpp
+++ week10/homework/jc12948_hw10_q3.cpp
@@ -1,26 +1,17 @@
+//This program stores a sequence of integers from an user input into an array for main1 and vector for main2.
+//It also asks the user for a numer they want to search for and prints out the position of the number.
 //Jacky Choi NYU Tandon Summer 2023
-// Question 3:
-// In this question, you will write two versions of a program that reads from the user a sequence of positive integers ending with -1, and another positive integer num that the user wishes to search for.
-// The program should then print all the line numbers in sequence entered by the user, that contain num, or a message saying that num does not show at all in the sequence. 13 5 8 2 9 5 8 8 -1
-// Please enter a number you want to search. 5
-// 5 shows in lines 2, 6.
-// a) The first version of the program, is not allowed to use the vector data structure.
-// b) The second version of the program, should use the vector data structure.
-// Implementation requirements (for both programs):
-// 1. Think how to break down your implementation to functions.
-// 2. Your programs should run in linear time. That is, if there are n numbers in the input
-// sequence, your program should run in 𝜃(𝑛).
-// 3. Write the two programs in two functions named main1() and main2(). Also have
-// the main() test these two functions.
+
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
 void main1();
+void main2();
 
 int main() {
-    main1();
+    main2();
     return 0;
 }
 
@@ -52,6 +43,9 @@ void main1() { //This function does not use vector
     if(found == true) { //set to true to start printing where nums are found
         cout<<input<<" is found in line(s): ";
     }
+    else {
+        cout<<input<<" is not found anywhere in the sequence.";
+    }
     for(int i = 0; i < size; i++) {
         if(arr[i] == input) {
             cout<<i+1<<" ";
@@ -61,4 +55,22 @@ void main1() { //This function does not use vector
 
     delete[] arr;
     arr = NULL;
+}
+
+void main2() {
+    vector<int> nums;
+    
+    int input = 0;
+    cout<<"Please enter a sequence of integers and -1 to stop: "<<endl;
+    while(input != -1) {
+        cin>>input;
+        if(input == -1) {
+            break;
+        }
+        nums.push_back(input);
+    }
+
+    for(int i = 0; i < nums.size(); i++) {
+        cout<<nums[i]<<endl;
+    }
 }
