85c9acb5e7716b15e27a525230e363067d99d9c6
Jacky <jchoi53@mail.ccsf.edu>
2023-10-05 22:30:52 -0700

    fix output syntax

--- week12/homework/jc12948_hw12.cpp
+++ week12/homework/jc12948_hw12.cpp
@@ -9,10 +9,10 @@ class Money {
     long all_cents;
 
 public:
+    friend Money operator -(const Money& amount);
     // friend Money operator *(const Money& amount1, const Money& amount2);
     friend Money operator +(const Money& amount1, const Money& amount2);
     friend Money operator -(const Money& amount1, const Money& amount2);
-    friend Money operator -(const Money& amount);
     friend bool operator ==(const Money& amount1, const Money& amount2);
     friend bool operator <(const Money& amount1, const Money& amount2);
     Money(long dollars, int cents);
@@ -121,7 +121,7 @@ int main() {
     cout<<endl;
     cout<<"Your new balance is: "<<new_balance<<endl;
     cout<<"The bank's new balance is: "<<new_bank_balance<<endl;
-    cout<<"The difference between your balance and the bank is a total of "<<balance_difference<<" dollars."<<endl;
+    cout<<"The difference between your balance and the bank is a total of "<<balance_difference<<endl;
     cout<<"List of cashed checks: "<<endl;
      for(int i = 0; i < num_checks; i++) {
         if(checks[i].get_cashed() == true) {
@@ -144,30 +144,11 @@ int digit_to_int(char c) {
     return static_cast<int>(c) - static_cast<int>('0');
 }
 
-Money::Money(long dollars, int cents) {
-    if (dollars * cents < 0) {
-        cout<<"Illegal values for dollars and cents.\n";
-        exit(1);
-    }
-
-    all_cents = dollars * 100 + cents;
-}
-
-Money::Money(long dollars) : all_cents(dollars * 100) {}
-
-Money::Money() : all_cents(0) {}
-
-int digit_to_int(char c);
-//Used in the definition of the overloaded input operator >>.
-//Precondition: c is one of the digits '0' through '9'.
-//Returns the integer for the digit; for example, digit_to_int('3') returns 3.
-
 Money operator +(const Money& amount1, const Money& amount2) {
     Money temp;
     temp.all_cents = amount1.all_cents + amount2.all_cents;
     return temp;
 }
-
 Money operator -(const Money& amount1, const Money& amount2) {
     Money temp;
     temp.all_cents = amount1.all_cents - amount2.all_cents;
@@ -179,19 +160,15 @@ Money operator -(const Money& amount) {
     temp.all_cents = -amount.all_cents;
     return temp;
 }
-
 bool operator ==(const Money& amount1, const Money& amount2) {
     return (amount1.all_cents == amount2.all_cents);
 }
-
 bool operator <(const Money& amount1, const Money& amount2) {
     return (amount1.all_cents < amount2.all_cents);
 }
-
 double Money::get_value() const {
     return all_cents * 0.01;
 }
-
 istream& operator >>(istream& ins, Money& amount) {
     char one_char, decimal_point, digit1, digit2;
     long dollars;
@@ -230,20 +207,16 @@ ostream& operator <<(ostream& outs, const Money& amount) {
     positive_cents = labs(amount.all_cents);
     dollars = positive_cents / 100;
     cents = positive_cents % 100;
-
     if (amount.all_cents < 0 ) {
         outs<<"-$"<<dollars<<'.';
     }
     else {
         outs<<"$"<<dollars<<'.';
     }
-
     if (cents < 10) {
         outs<<'0';
     }
-
     outs<<cents;
-
     return outs;
 }
 
@@ -268,10 +241,19 @@ void Check::set_cashed(bool cashed) {
 }
 
 Check::Check(int number, long dollars, int cents, bool cashed) : number(number), amount(dollars, cents), is_cashed(cashed) {};
-
 Check::Check(int number, long dollars, bool cashed) : number(number), amount(dollars, 0), is_cashed(cashed) {};
-
 Check::Check() : number(0), amount(0, 0), is_cashed(false) {};
+
+Money::Money(long dollars) : all_cents(dollars * 100) {}
+Money::Money() : all_cents(0) {}
+Money::Money(long dollars, int cents) {
+    if (dollars * cents < 0) {
+        cout<<"Illegal values for dollars and cents.\n";
+        exit(1);
+    }
+
+    all_cents = dollars * 100 + cents;
+}
 
 Money get_amount_cashed_checks(const Check checks[], int num_checks) {
     Money amount;
