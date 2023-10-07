f8a8eef0f77e164a271b8088caf6ad68691a0d44
Jacky <jchoi53@mail.ccsf.edu>
2023-10-06 19:59:40 -0700

    add world display with empty spaces

--- week13/homework/jc12948_hw13.cpp
+++ week13/homework/jc12948_hw13.cpp
@@ -3,26 +3,44 @@
 #include <iostream>
 using namespace std;
 
-class World;
+class World {
+public:
+    void display() const{
+        for(int i = 0; i < 20; i++) {
+            for(int j = 0; j < 20; j++) {
+                cout<<" - ";
+            }
+            cout<<endl<<endl;
+        }
+    }
+};
 class Organism;
-class DoodleBug;
+class Doodlebug;
 class Ant;
+
+//move
+//breed
+//starve
+//pass_away
 
 
 int main() {
     int time_count = 1;
     string input;
+    World world;
     cout<<"Please press enter to continue with the time simulation or any enter any else to stop: "<<endl;
-    cin.get();
+    getline(cin, input);
     while(true) {
         cout<<"Time: "<<time_count<<endl;
         getline(cin, input);
         if(input == "") {
+            world.display();
             time_count++;
         }
         else {
             break;
         }
+
     }
     return 0;
 }
