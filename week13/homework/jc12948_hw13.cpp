85ef6f9e87c6798f69c9a54715e140ff681401b1
Jacky <jchoi53@mail.ccsf.edu>
2023-10-08 16:57:16 -0700

    add members and functions

--- week13/homework/jc12948_hw13.cpp
+++ week13/homework/jc12948_hw13.cpp
@@ -1,27 +1,76 @@
-//Doodlebug
-
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
 using namespace std;
 
-class World {
-public:
-    void display() const{
-        for(int i = 0; i < 20; i++) {
-            for(int j = 0; j < 20; j++) {
-                cout<<" - ";
-            }
-            cout<<endl<<endl;
-        }
-    }
-};
+class World;
 class Organism;
 class Doodlebug;
 class Ant;
 
-//move
-//breed
-//starve
-//pass_away
+class World {
+    friend class Ant;
+    friend class Organism;
+    friend class Doodlebug;
+    int time_step;
+
+public:
+    vector<Organism> world;
+    vector<Ant> ants;
+    void display();
+    void move_doodlebugs(); 
+    void move_ants();
+    void breed_doodlebugs(); 
+    void breed_ants();
+    void starve();  
+    int next_time_step(); //increment time step by 1
+    void set_time_step();
+};
+
+
+class Organism {
+    int location;
+    char type;
+public:
+    void set_type(char curr_type);
+    char get_type();
+    void move();
+    void breed();
+    void starve();
+    int get_location();
+    void set_location();
+
+    Organism(){};
+};
+
+class Ant: public Organism {
+    int ant_location;
+    char type = 'o';
+public:
+    void move();   //find adjacent empty spots and randomly move every time step, if out of bounds or all adjacent full, stay still
+    void breed(); //find adjacent empty spots and breed every 3 time steps. 
+    int get_ant_location();
+    void set_ant_location();
+
+    Ant(){};
+};
+
+class Doodlebug: public Organism {
+    int doodlebug_location;
+    char type = 'x';
+    int starve_count = 0;
+public:
+    void move();   //find adjacent empty spots and randomly move every time step
+    void breed(); //find adjacent empty spots and breed every 8 time steps
+    void starve(); //3 time steps and doodle starves and dies
+    int get_starve_count(); 
+    int get_doodlebug_location();
+    void set_doodlebug_location();
+
+    Doodlebug(){};
+};
 
 
 int main() {
@@ -43,4 +92,13 @@ int main() {
 
     }
     return 0;
+}
+
+void World::display() {
+    for(int i = 0; i < 20; i++) {
+        for(int j = 0; j < 20; j++) {
+            cout<<" - ";
+        }
+        cout<<endl<<endl;
+    }
 }
