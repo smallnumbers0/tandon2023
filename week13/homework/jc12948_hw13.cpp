e0eb7279580a698b27b259af6def74305dbbddd3
Jacky <jchoi53@mail.ccsf.edu>
2023-10-09 08:06:28 -0700

    remove virtual function to test world is working

--- week13/homework/jc12948_hw13.cpp
+++ week13/homework/jc12948_hw13.cpp
@@ -19,39 +19,43 @@ class World {
 public:
     vector<Organism> world;
     vector<Ant> ants;
-    void display();
+    vector<Doodlebug> doodlebugs;
+
+    void display() const;
     void move_doodlebugs(); 
     void move_ants();
     void breed_doodlebugs(); 
     void breed_ants();
     void starve();  
-    int next_time_step(); //increment time step by 1
+    int get_time_step() const; //increment time step by 1
     void set_time_step();
+
+    World();
 };
 
 
 class Organism {
     int location;
-    char type;
+    char type = '-';
 public:
-    void set_type(char curr_type);
-    char get_type();
-    virtual void move(); 
-    virtual void breed();
-    int get_location();
-    void set_location();
+    void set_type(char new_type);
+    char get_type() const;
+    void move(); 
+    void breed();
+    int get_location() const;
+    void set_location(int new_location);
 
-    Organism(){};
+    Organism(){type = '-';};
 };
 
 class Ant: public Organism {
     int ant_location;
     char type = 'o';
 public:
-    void move();   //find adjacent empty spots and randomly move every time step, if out of bounds or all adjacent full, stay still
-    void breed(); //find adjacent empty spots and breed every 3 time steps. 
-    int get_ant_location();
-    void set_ant_location();
+    void move(vector<Organism> &world);   //find adjacent empty spots and randomly move every time step, if out of bounds or all adjacent full, stay still
+    void breed(vector<Organism> &world, vector<Ant> &ants, int &time_step); //find adjacent empty spots and breed every 3 time steps. 
+    int get_ant_location() const;
+    void set_ant_location(int new_location);
 
     Ant(){};
 };
@@ -61,12 +65,12 @@ class Doodlebug: public Organism { //derived classes can overwrite virtual funct
     char type = 'x';
     int starve_count = 0;
 public:
-    void move();   //find adjacent empty spots and randomly move every time step
-    void breed(); //find adjacent empty spots and breed every 8 time steps
-    void starve(); //3 time steps and doodle starves and dies
+    void move(vector<Organism> &world, vector<Ant> &ants);   //find adjacent empty spots and randomly move every time step, if there are ants, it will eat the ant instead
+    void breed(vector<Organism> &world, vector<Doodlebug> &doodlebugs, int time_step); //find adjacent empty spots and breed every 8 time steps
+    void starve(vector<Organism> &world, vector<Doodlebug> &doodlebugs); //3 time steps and doodle starves and dies
     int get_starve_count(); 
-    int get_doodlebug_location();
-    void set_doodlebug_location();
+    int get_doodlebug_location() const;
+    void set_doodlebug_location(int new_location);
 
     Doodlebug(){};
 };
@@ -92,12 +96,107 @@ int main() {
     }
     return 0;
 }
+/******************************************************/
+/*****************  World Simulation  *****************/
+/******************************************************/
 
-void World::display() {
+World::World() {
+    int location = 0;
+    for(int i = 1; i <= 400; i++) {
+        world.push_back(Organism());
+        world[location].set_location(location);
+        location++;
+    }
+}
+
+void World::display() const {
+    int location = 0;
     for(int i = 0; i < 20; i++) {
         for(int j = 0; j < 20; j++) {
-            cout<<"-"<<" ";
+            if(world[location].get_type() == '-') {
+                cout<<'-';
+            }
+            else if(world[location].get_type() == 'o') {
+                cout<<'o';
+            }
+            else if(world[location].get_type() == 'x') {
+                cout<<'x';
+            }
+            location++;
         }
         cout<<endl;
     }
 }
+
+/******************************************************/
+/*****************  General Organism  *****************/
+/******************************************************/
+
+void Organism::set_type(char new_type) {
+    type = new_type; 
+}
+
+char Organism::get_type() const{
+    return type;
+}
+
+int Organism::get_location() const {
+    return location;
+}
+
+void Organism::set_location(int new_location) {
+    location = new_location;
+}
+
+/******************************************************/
+/***********************  Ant  ************************/
+/******************************************************/
+
+int Ant::get_ant_location() const {
+    return ant_location;
+}
+
+void Ant::set_ant_location(int new_location) {
+    ant_location = new_location;
+}
+
+// void Ant::move(vector<Organism> &world) { // -1 for left/ +1 for right/ -20 for up/ + 20 for down
+
+// }
+
+// void Ant::breed(vector<Organism> &world, vector<Ant> &ants, int &time_step) {
+    
+// }
+
+/******************************************************/
+
+/******************************************************/
+/*******************  Doodle Bug  *********************/
+/******************************************************/
+
+int Doodlebug::get_doodlebug_location() const {
+    return doodlebug_location;
+}
+
+void Doodlebug::set_doodlebug_location(int new_location) {
+    doodlebug_location = new_location;
+}
+
+int Doodlebug::get_starve_count() {
+    return starve_count;
+}
+
+// void Doodlebug::starve(vector<Organism> &world, vector<Doodlebug> &doodlebugs) {
+
+// }
+
+// void Doodlebug::move(vector<Organism> &world, vector<Ant> &ants) {
+
+// }
+
+// void Doodlebug::breed(vector<Organism> &world, vector<Doodlebug> &Doodlebug, int time_step) {
+
+// }
+
+/******************************************************/
+
